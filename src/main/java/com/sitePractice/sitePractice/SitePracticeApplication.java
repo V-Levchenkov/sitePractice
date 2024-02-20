4dc1e431068a39d46b9ad91801f398b0ae7d4747
v.levchenkov <v.levchenkovv@Gmail.com>
2024-02-20 13:48:56 +0300

    Initial commit

---
+++ src/main/java/com/sitePractice/sitePractice/SitePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sitePractice.sitePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitePractice/sitePractice/SitePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitePractice.sitePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
